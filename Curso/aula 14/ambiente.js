1168f5669293db6c41a1380c33396ca6cbab8849
Daniel Siqueira <149271743+danieldevbz39@users.noreply.github.com>
2025-05-21 20:16:50 -0300

    21-05-2025

---
+++ Curso/aula 14/ambiente.js
@@ -0,0 +1,21 @@
+var c = 1 /* c recebe 1 */
+while (c <= 5) { /* enquanto c for menor igual a 5 */
+    console.log(c) /*conte*/
+    c++ /*c recebe + 1 */
+}
+
+var c = 1 /* c recebe 1 */
+do { /*faça*/
+    console.log(c) /*conte*/
+    c++ /*c recebe + 1 */
+} while (c <= 6)  /* enquanto c for menor igual a 5 */
+
+for (var c = 1;c <= 3;c++) { /* */
+    console.log(c) /* */
+}
+
+console.log('Vai começar...')
+for(var c = 1;c <= 4;c++) {
+    console.log(c)
+}
+console.log('FIM!')
---
+++ Curso/aula13/ambienteteste.js
@@ -0,0 +1,12 @@
+/*var c = 1
+while (c <= 6) { /*Enquanto contador for menor igual a 6*/
+    console.log(`${c} Tudo bem ? `)
+    c++ /*c recebe c + 1*/
+
+
+var c = 1
+do { //faça isso
+    console.log(`${c} Tudo bem ? `)
+    c++ /*c recebe c + 1*/
+} while (c <= 6) /*Enquanto contador for menor igual a 6*/
+
