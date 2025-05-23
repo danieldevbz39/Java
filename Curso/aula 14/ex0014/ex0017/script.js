56e1335f064c93d64507d799f8c1bc01e4ab18ed
Daniel Siqueira <149271743+danieldevbz39@users.noreply.github.com>
2025-05-23 16:06:42 -0300

    tabuada

--- Curso/aula 14/ex0014/ex0017/script.js
+++ Curso/aula 14/ex0014/ex0017/script.js
@@ -1,1 +1,19 @@
-alert('Olá!')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) { //se num estiver vazio
+        window.alert('Por favor, digite um número! ')        
+    } else { //se não
+        let n = Number(num.value) //recebe numbero
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }       
+    }
+
+}
