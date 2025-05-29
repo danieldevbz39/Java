9bb4540f43390b0d1e86bc71c23b0a7334557ea2
Daniel Siqueira <149271743+danieldevbz39@users.noreply.github.com>
2025-05-28 23:11:51 -0300

    Ola mundo
    Primeiro programa

--- PrimeiroPrograma/src/main/java/com/mycompany/primeiroprograma/PrimeiroPrograma.java
+++ PrimeiroPrograma/src/main/java/com/mycompany/primeiroprograma/PrimeiroPrograma.java
@@ -1,16 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.primeiroprograma;
 
 /**
  *
  * @author daniel
  */
-public class PrimeiroPrograma {
+public class PrimeiroPrograma { 
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) {//psvm+ tab exibe o código
+        System.out.println("Ola Mundo!");
     }
 }
